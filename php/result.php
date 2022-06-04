837849c80e4f5f52ef0935f9fb55eca69bd45883
tomita-dotcom <springroll1816@gmail.com>
2022-06-04 18:09:33 +0900

    6-4-2

--- php/result.php
+++ php/result.php
@@ -1,12 +1,10 @@
 <?php
 
-$name = htmlspecialchars($_POST['name'], ENT_QUOTES);
-print"私の名前は、".$name."<br/>";
-$product = htmlspecialchars($_POST['product'], ENT_QUOTES);
-$order = htmlspecialchars($_POST['order'], ENT_QUOTES);
+$my_name = htmlspecialchars($_POST['my_name'], ENT_QUOTES);
+$choices = htmlspecialchars($_POST['choices'], ENT_QUOTES);
+$number = htmlspecialchars($_POST['number'], ENT_QUOTES);
 ?>
 
-<p>私の名前は、<?php echo $name; ?></p>
-<p>ご希望商品は、<?php echo $product; ?></p>
-<p>注文数は、<?php echo $order; ?></p>
-
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>ご希望商品は、<?php echo $choices; ?></p>
+<p>注文数は、<?php echo $number; ?></p>
